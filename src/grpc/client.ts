9794e5fd39b5926bf4db1182671c4d945b47d3dd
Aint <pinevichv@gmail.com>
2023-10-30 21:12:04 +0300

    testing stream con

--- src/grpc/client.ts
+++ src/grpc/client.ts
@@ -6,14 +6,14 @@ const services = require('./proto/model_grpc_pb');
 let client: ServiceClient;
 const getConnect = () => {
     console.log(grpc.status);
-    if (grpc.status !== grpc.status.OK) {
-        client.close();
-        client = new services.BotChatClient(
-            'chat:8082',
-            grpc.credentials.createInsecure(),
-        );
-        client.waitForReady(5000, (() => console.log('lol')));
-    }
+    // if (grpc.status !== grpc.status.OK) {
+    // client.close();
+    client = new services.BotChatClient(
+        'chat:8082',
+        grpc.credentials.createInsecure(),
+    );
+    client.waitForReady(5000, (() => console.log('lol')));
+    // }
     return client;
 };
 
