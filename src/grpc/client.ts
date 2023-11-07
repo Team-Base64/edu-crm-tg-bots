543ce4d9b8fb78fd90f33460bf1c0d464d1977f8
EuphoriaAbsorber <Scorpion1remeres@gmail.com>
2023-11-07 23:14:47 +0300

    grpc connection

--- src/grpc/client.ts
+++ src/grpc/client.ts
@@ -2,7 +2,8 @@ const grpc = require('@grpc/grpc-js');
 const services = require('./proto/model_grpc_pb');
 
 const client = new services.BotChatClient(
-    '127.0.0.1:8082',
+    //'127.0.0.1:8082',
+    `host.docker.internal:8082`,
     grpc.credentials.createInsecure(),
 );
 
--- src/masterBot/netMasterBot.ts
+++ src/masterBot/netMasterBot.ts
@@ -1,4 +1,7 @@
 import MasterBot from './masterBot';
+import client from '../grpc/client';
+const messages = require('../grpc/proto/model_pb');
+import { logger } from '../utils/logger';
 
 export class NetMasterBot {
     masterBot: MasterBot;
@@ -18,10 +21,48 @@ export class NetMasterBot {
      * @returns isvalid, classid
      * */
     verifyToken(token: string) {
-        if (token === '12345679') return { isvalid: true, classid: 2 };
-        if (token === '12345678') return { isvalid: true, classid: 3 };
-        if (token === '12345670') return { isvalid: true, classid: 4 };
-        return { isvalid: true, classid: 1 };
+        // const func1 = (err: string, response: number) => {
+        //     if (err) {
+        //         logger.error('Error:  ', err);
+        //         return { isvalid: false, classid: -1 };
+        //     }
+        //     logger.info('verifyToken resp ' + response);
+        //     return { isvalid: true, classid: response };
+        // };
+        // const newfunc = func1.bind(this);
+        // // if (token === '12345679') return { isvalid: true, classid: 2 };
+        // // if (token === '12345678') return { isvalid: true, classid: 3 };
+        // // if (token === '12345670') return { isvalid: true, classid: 4 };
+        // logger.info('verifyToken ' + token);
+        // const request = new messages.ValidateTokenRequest();
+        // request.setToken(token);
+        // const result = client.validateToken(request);
+        // logger.info('verifyToken result ' + result);
+        // return result;
+
+        // if (token === '12345679') return { isvalid: true, classid: 2 };
+        // if (token === '12345678') return { isvalid: true, classid: 3 };
+        // if (token === '12345670') return { isvalid: true, classid: 4 };
+        logger.info('verifyToken ' + token);
+        const request = new messages.ValidateTokenRequest();
+        request.setToken(token);
+        let a = false,
+            b = -1;
+        client.validateToken(request, (err: string, response: number) => {
+            if (err) {
+                logger.error('Error:  ', err);
+                //return { isvalid: false, classid: -1 };
+                return;
+            }
+            logger.info('verifyToken resp ' + response);
+            a = true;
+            b = response;
+            //return { isvalid: true, classid: response };
+        });
+        //logger.info('verifyToken result ' + JSON.stringify(result));
+        logger.info('verifyToken result ' + a + ' ' + b);
+        return { isvalid: a, classid: b };
+        //return result;
     }
 
     /**
@@ -29,10 +70,25 @@ export class NetMasterBot {
      * @returns chatid
      * */
     createChat(studentid: number, classid: number) {
-        if (classid === 2) return 2;
-        if (classid === 3) return 3;
-        if (classid === 4) return 4;
-        return 1;
+        // if (classid === 2) return 2;
+        // if (classid === 3) return 3;
+        // if (classid === 4) return 4;
+        logger.info('createChat ' + studentid + ' ' + classid);
+        const request = new messages.CreateChatRequest();
+        request.setStudentid(studentid);
+        request.setClassid(classid);
+        const result = client.createChat(
+            request,
+            (err: string, response: number) => {
+                if (err) {
+                    logger.error('Error:  ', err);
+                    return -1;
+                }
+                logger.info('createChat resp ' + response);
+                return response;
+            },
+        );
+        return result;
     }
 
     /**
@@ -40,6 +96,24 @@ export class NetMasterBot {
      * @returns studentid
      * */
     register(name: string, avatar: string) {
-        return 1;
+        //return 1;
+        logger.info('register ' + name + ' ' + avatar);
+        const tg = 'tg';
+        const request = new messages.CreateStudentRequest();
+        request.setName(name);
+        request.setType(tg);
+        request.setAvatarurl(avatar);
+        const result = client.createStudent(
+            request,
+            (err: string, response: number) => {
+                if (err) {
+                    logger.error('Error:  ', err);
+                    return -1;
+                }
+                logger.info('register resp ' + response);
+                return response;
+            },
+        );
+        return result;
     }
 }
