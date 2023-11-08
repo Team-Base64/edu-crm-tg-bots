73573495ef2e15548b447e2f40a1a37604d84926
EuphoriaAbsorber <Scorpion1remeres@gmail.com>
2023-11-08 19:39:20 +0300

    fix before pk3

--- db/populate.sql
+++ db/populate.sql
@@ -3,9 +3,10 @@ INSERT INTO
 VALUES
     (
         '1064016468:AAEaJJWW0Snm_sZsmQtgoEFbUTYj6pM60hk',
-        'https://t.me/Aintttbot'
+        'https://t.me/GG222bot'
+        
     ),
     (
         '1290980811:AAEgopVWqb7o0I72cwdIGGZRsRyE0GGNkLA',
-        'https://t.me/GG222bot'
+        'https://t.me/Aintttbot'
     );
