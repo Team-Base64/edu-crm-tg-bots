e3c9832a343f1e5675cd907649339012d6d3dbac
Aint <pinevichv@gmail.com>
2023-11-14 22:11:00 +0300

    removed tg tokens

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,8 @@ import { Store } from './repository/store';
 import postgresConfig from './repository/config';
 import NetSlaveBot from './slaveBot/netSlaveBot';
 import { NetMasterBot } from './masterBot/netMasterBot';
+require('dotenv').config();
 
-// const slaveTokens = [
-//     '1064016468:AAEaJJWW0Snm_sZsmQtgoEFbUTYj6pM60hk',
-//     '1290980811:AAEgopVWqb7o0I72cwdIGGZRsRyE0GGNkLA',
-// ];
 export const masterBotTokenLength = 8;
 
 // time in seconds
@@ -17,7 +14,7 @@ export const dbInstance = new Store(postgresConfig);
 
 export const netSlaveBotInstance = new NetSlaveBot();
 
-const masterBotToken = '6881067197:AAHLj70waoWo5PnS009QYyy8U3ka9SuZhWg';
+const masterBotToken = process.env.MASTER_BOT_TOKEN as string;
 
 export const netMasterBotInstance = new NetMasterBot(masterBotToken);
 
