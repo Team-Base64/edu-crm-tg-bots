e9a56eb1a0b4b7ae4f40f2933ee20867b57c676b
EuphoriaAbsorber <Scorpion1remeres@gmail.com>
2023-11-21 14:29:38 +0300

    microfix

--- src/grpc/client.ts
+++ src/grpc/client.ts
@@ -3,9 +3,8 @@ const services = require('./proto/model_grpc_pb');
 require('dotenv').config();
 
 const client = new services.BotChatClient(
-    //process.env.CLIENT_HOST_TG_BOT,
-    //'127.0.0.1:8082',
-    `host.docker.internal:8082`,
+    process.env.CLIENT_HOST_TG_BOT,
+    //`host.docker.internal:8082`,
     grpc.credentials.createInsecure(),
 );
 
